e5a1765e4d4a0078d0f2442c9562deb7737d3403
brlaney42 <brlaney@outlook.com>
2021-03-28 21:08:40 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import PhotoCamera from '@material-ui/icons';
+
+const App = () => {
+  return (
+    <div>
+      <Typography varient="h1">Hello, world!</Typography>
+    </div>
+  );
+}
+
+export default App;
